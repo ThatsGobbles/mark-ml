bbe564b692cc2f4bbad80db9756b65674ccf78f8
Mark LeMoine <thatsgobbles@gmail.com>
2019-12-09 16:54:13 -0800

    Adding barebones `error` module

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,65 @@
-pub enum Error {}
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+// This is a bare-bones implementation. A real library would provide additional
+// information in its error type, for example the line and column at which the
+// error occurred, the byte offset into the input, or the current key being
+// processed.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Error {
+    // One or more variants that can be created by data structures through the
+    // `ser::Error` and `de::Error` traits. For example the Serialize impl for
+    // Mutex<T> might return an error because the mutex is poisoned, or the
+    // Deserialize impl for a struct may return an error because a required
+    // field is missing.
+    Message(String),
+
+    // Zero or more variants that can be created directly by the Serializer and
+    // Deserializer without going through `ser::Error` and `de::Error`. These
+    // are specific to the format, in this case JSON.
+    Eof,
+    // Syntax,
+    // ExpectedBoolean,
+    // ExpectedInteger,
+    // ExpectedString,
+    // ExpectedNull,
+    // ExpectedArray,
+    // ExpectedArrayComma,
+    // ExpectedArrayEnd,
+    // ExpectedMap,
+    // ExpectedMapColon,
+    // ExpectedMapComma,
+    // ExpectedMapEnd,
+    // ExpectedEnum,
+    // TrailingCharacters,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::Message(ref s) => write!(f, "{}", s),
+            _ => write!(f, "an error occured"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            Error::Message(..) => None,
+            _ => None,
+        }
+    }
+}
+
+impl serde::ser::Error for Error {
+    fn custom<T: std::fmt::Display>(msg: T) -> Self {
+        Error::Message(msg.to_string())
+    }
+}
+
+impl serde::de::Error for Error {
+    fn custom<T: std::fmt::Display>(msg: T) -> Self {
+        Error::Message(msg.to_string())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-mod de;
 mod error;
 mod ser;
+mod de;
 
 pub use error::{Error, Result};
-// pub use de::{from_str, Deserializer};
 // pub use ser::{to_string, Serializer};
+// pub use de::{from_str, Deserializer};
